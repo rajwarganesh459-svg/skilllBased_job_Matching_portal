4ea0a3c1a58eb754cced939bd4a093555d084d64
Ganesh Rajwar <your-email@gmail.com>
2026-05-27 14:30:11 +0530

    added filess.io mysql

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ app.secret_key = "secret123"
 
 def get_db():
     return mysql.connector.connect(
-        host="sql208.infinityfree.com",
-        user="if0_42029111",
-        password="UNHlydT1m2vo",
-        database="if0_42029111_jobportal",
-        port=3306
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT"))
     )
 # ---------------- HOME ----------------
 @app.route('/')
